d699c5359c242da89b61eff620cb2a1e94f494ff
Vyacheslav <slavochka-gladkov@mail.ru>
2024-02-01 21:35:22 +0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+List<string> words = new List<string>() {
+    "Bella", "Alex", "John"
+};
+words.Add("Mike");
+words.Add("Bob");
+words.Add("Mario");
+foreach (string name in words)
+{
+    Console.WriteLine("Name: " + name);
+}
